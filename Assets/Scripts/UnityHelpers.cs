561872a99166d4190237a097ecd49c2e733c12d2
Mirza Brunjadze <brunjadzem@gmail.com>
2018-09-21 21:10:12 +0400

    done

--- Assets/Scripts/UnityHelpers.cs
+++ Assets/Scripts/UnityHelpers.cs
@@ -4,6 +4,7 @@ public static class UnityHelpers
 {
     public static bool IsIPhoneX()
     {
-        return UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneX;
+        //return UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneX;
+        return false;
     }
 }
